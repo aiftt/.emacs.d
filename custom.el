6cda94773c32a1bce9d197638fceb58e50475af6
aiftt <ftt.loves@gmail.com>
2024-07-09 17:19:07 +0800

    2024-07-09 17:19:07

--- custom.el
+++ custom.el
@@ -3,14 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-safe-remote-resources
-   '("\\`https://aiftt\\.github\\.io/org-html-themes/org/theme-readtheorg\\.setup\\'"))
- '(package-selected-packages
-   '(flycheck lua-mode pkg-info sql-indent php-mode dockerfile-mode go-mode python-mode yaml-imenu evil-nerd-commenter blamer youdao-dictionary fanyi doom-themes all-the-icons-dired all-the-icons-completion parrot string-inflection emacs-maple-iedit maple-iedit toggle-quotes-plus move-text doom-modeline highlight-symbol corfu embark-consult embark vertico hydra discover-my-major exec-path-from-shell diminish dired modalka restart-emacs org-contrib))
- '(package-vc-selected-packages
-   '((blamer :vc-backend Git :url "https://www.github.com/Artawower/blamer.el")
-     (emacs-maple-iedit :vc-backend Git :url "https://www.github.com/honmaple/emacs-maple-iedit")
-     (toggle-quotes-plus :vc-backend Git :url "https://www.github.com/jcs-elpa/toggle-quotes-plus")))
  '(safe-local-variable-values
    '((eval when
 	   (fboundp #'tangle-if-init)
--- early-init.el
+++ early-init.el
@@ -1,4 +1,4 @@
 (setq package-enable-at-startup nil)
-  (setenv "LSP_USE_PLISTS" "true")
-  (provide 'early-init)
+(setenv "LSP_USE_PLISTS" "true")
+(provide 'early-init)
 ;;; early-init.el ends here
