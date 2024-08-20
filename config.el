a68a9ed9283bf99fd27edea3011a94c41a9104b3
aiftt <ftt.loves@gmail.com>
2024-08-20 10:02:26 +0800

    2024-08-20 10:02:26

--- config.el
+++ config.el
@@ -150,82 +150,99 @@
 ;; 关闭 Edebug 日志输出
 (setq edebug-trace nil)
 
-;; 去掉工具栏等
-(when window-system
-  (tool-bar-mode 0)
-  (scroll-bar-mode 0)
-  (tooltip-mode 0))
+(setq debug-on-error nil)
+  (setq edebug-all-defs nil)
+  (setq edebug-all-forms nil)
 
-;; 高亮当前行
-(global-hl-line-mode 1)
-;; 显示列号
-(column-number-mode t)
+        ;; 去掉工具栏等
+        (when window-system
+          (tool-bar-mode 0)
+          (scroll-bar-mode 0)
+          (tooltip-mode 0))
 
-;; 启动全屏
-(set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+        ;; 高亮当前行
+        (global-hl-line-mode 1)
+        ;; 显示列号
+        (column-number-mode t)
 
-;; 启动界面
-(setq inhibit-startup-message t
-      initial-scratch-message ""
-      initial-major-mode 'emacs-lisp-mode
-      initial-buffer-choice t
-      inhibit-splash-screen t)
+        ;; 启动全屏
+        (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 
-;; - 选中粘贴时能覆盖选中的内容
-(delete-selection-mode 1)
+        ;; 启动界面
+        (setq inhibit-startup-message t
+              initial-scratch-message ""
+              initial-major-mode 'emacs-lisp-mode
+              initial-buffer-choice t
+              inhibit-splash-screen t)
 
-;; - 文件编码
-(prefer-coding-system 'utf-8)
-(setq-default buffer-file-coding-system 'utf-8-auto-unix)
+        ;; - 选中粘贴时能覆盖选中的内容
+        (delete-selection-mode 1)
 
-;; - 错误信息
-(setq visible-bell nil)
-(setq ring-bell-function 'ignore)
+        ;; - 文件编码
+        (prefer-coding-system 'utf-8)
+      ;; 设置 Emacs 的默认编码为 UTF-8
+    (prefer-coding-system 'utf-8)
+    (set-default-coding-systems 'utf-8)
+    ;; (set-terminal-coding-system 'utf-8)
+    ;; (set-keyboard-coding-system 'utf-8)
+    (setq-default buffer-file-coding-system 'utf-8)
+    ;; (setq coding-system-for-read 'utf-8)
+    ;; (setq coding-system-for-write 'utf-8)
 
-;; - 截断行
-(setq-default truncate-lines t)
-;; - yes or no
-(fset 'yes-or-no-p 'y-or-n-p)
-;; - 驼峰单词里移动
-(subword-mode)
-;; - 默认认为两个空格开头为一个段落，关闭此选项
-(setq sentence-end-double-space nil)
-;; - 更好的通配符搜索
-(setq search-whitespace-regexp ".*?")
-;; - 窗口管理
-(when (fboundp 'winner-mode)
-  (winner-mode 1))
+    ;; 如果使用 Windows 环境，可能还需要添加以下内容
+    ;; (set-language-environment "UTF-8")
+        ;; (setq-default buffer-file-coding-system 'utf-8-auto-unix)
+      ;;  (setq-default coding-system-for-read 'utf-8-unix)
+      ;; (setq-default coding-system-for-write 'utf-8-unix)
 
-;; - 在 mac 上，当进入一个新的工作空间时，会默认全屏
-(setq ns-use-native-fullscreen nil)
+        ;; - 错误信息
+        (setq visible-bell nil)
+        (setq ring-bell-function 'ignore)
 
-;; - 不生成备份文件
-(setq make-backup-files nil)
+        ;; - 截断行
+        (setq-default truncate-lines t)
+        ;; - yes or no
+        (fset 'yes-or-no-p 'y-or-n-p)
+        ;; - 驼峰单词里移动
+        (subword-mode)
+        ;; - 默认认为两个空格开头为一个段落，关闭此选项
+        (setq sentence-end-double-space nil)
+        ;; - 更好的通配符搜索
+        (setq search-whitespace-regexp ".*?")
+        ;; - 窗口管理
+        (when (fboundp 'winner-mode)
+          (winner-mode 1))
 
-;; 启用自动保存已访问的文件 ss
-;; (auto-save-visited-mode 1)
-;; 设置自动保存的间隔时间
-;; (setq auto-save-visited-interval 1)  ; 每秒钟保存一次当前文件的备份
-;; (setq auto-save-interval 1)          ; 每秒钟保存一次所有文件的备份
-(setq save-silently t)  ; 自动保存文件，避免提示确认
-;; 分割窗口的时候自动切换到该窗口
-;; (add-hook 'window-setup-hook 'select-window)
+        ;; - 在 mac 上，当进入一个新的工作空间时，会默认全屏
+        (setq ns-use-native-fullscreen nil)
+
+        ;; - 不生成备份文件
+        (setq make-backup-files nil)
+
+        ;; 启用自动保存已访问的文件 ss
+        ;; (auto-save-visited-mode 1)
+        ;; 设置自动保存的间隔时间
+        ;; (setq auto-save-visited-interval 1)  ; 每秒钟保存一次当前文件的备份
+        ;; (setq auto-save-interval 1)          ; 每秒钟保存一次所有文件的备份
+        (setq save-silently t)  ; 自动保存文件，避免提示确认
+        ;; 分割窗口的时候自动切换到该窗口
+        ;; (add-hook 'window-setup-hook 'select-window)
 
 
-;; 有些功能需要用到，比如：折叠等等
-(add-hook 'prog-mode-hook #'hs-minor-mode)
+        ;; 有些功能需要用到，比如：折叠等等
+        (add-hook 'prog-mode-hook #'hs-minor-mode)
 
-;; (defun my-split-window-and-switch ()
-;;   "Split the window and switch to the newly created window."
-;;   (interactive)
-;;   (let ((current-window (selected-window)))
-;;     (call-interactively #'split-window)
-;;     (select-window (next-window current-window))))
+        ;; (defun my-split-window-and-switch ()
+        ;;   "Split the window and switch to the newly created window."
+        ;;   (interactive)
+        ;;   (let ((current-window (selected-window)))
+        ;;     (call-interactively #'split-window)
+        ;;     (select-window (next-window current-window))))
 
-;; (advice-add 'split-window :after #'my-split-window-and-switch)
+        ;; (advice-add 'split-window :after #'my-split-window-and-switch)
 
-(setq-default indent-tabs-mode nil) ; 不使用 TAB
-(setq-default tab-width 2) ; 设置全局 tab 宽度为 2
+        (setq-default indent-tabs-mode nil) ; 不使用 TAB
+        (setq-default tab-width 2) ; 设置全局 tab 宽度为 2
 
 (setq
  ;; 缩短更新 screen 的时间
@@ -1444,9 +1461,7 @@
 (use-package flycheck
   :diminish flycheck-mode
   :init (global-flycheck-mode))
-
-(use-package prettier)
-(add-hook 'after-init-hook #'global-prettier-mode)
+(setq flycheck-javascript-eslint-executable "eslint_d")
 
 (use-package highlight-parentheses
   :hook (prog-mode . highlight-parentheses-mode)
@@ -1627,42 +1642,16 @@
 ;; 打开日志，开发者才需要
 ;; (setq lsp-bridge-enable-log t)
 
-(defun eslint-fix-file ()
-  (interactive)
-  (message "eslint --fixing the file" (buffer-file-name))
-  (shell-command (concat "npx eslint --fix " (buffer-file-name))))
-
-(defun eslint-fix-file-and-revert ()
-  (interactive)
-  (eslint-fix-file)
-  (revert-buffer t t))
-
-(global-set-key (kbd "C-S-f") 'eslint-fix-file-and-revert)
-
-;; (add-hook 'web-mode-hook
-;;           (lambda ()
-;;             (add-hook 'after-save-hook #'eslint-fix-file-and-revert)))
-
-;; (add-hook 'typescript-ts-mode-hook
-;;           (lambda ()
-;;             (add-hook 'after-save-hook #'eslint-fix-file-and-revert)))
-
-;; (add-hook 'js2-mode-hook
-;;           (lambda ()
-;;             (add-hook 'after-save-hook #'eslint-fix-file-and-revert)))
-
-;; (add-hook 'typescript-tsx-mode-hook
-;;           (lambda ()
-;;             (add-hook 'after-save-hook #'eslint-fix-file-and-revert)))
-
-(use-package eslintd-fix
-  :hook ((js-mode . eslintd-fix-mode)
-         ;; (typescript-mode . eslintd-fix-mode)
-         ;; (vue-mode . eslintd-fix-mode)
-         (web-mode . eslintd-fix-mode)
-         (typescript-ts-mode . eslintd-fix-mode)
-         (typescript-tsx-mode . eslintd-fix-mode)
-         ))
+(use-package reformatter
+    :config
+    (progn
+;;;###autoload (autoload 'eslintd-fix "current-file" nil t)
+;;;###autoload (autoload 'eslintd-fix-on-save-mode "current-file" nil t)
+      (reformatter-define eslintd-fix
+        :program (executable-find "eslint_d")
+        :args (list "--fix-to-stdout" "--stdin" "--stdin-filename" (buffer-file-name))
+        :input-file (reformatter-temp-file-in-current-directory "js")
+        :exit-code-success-p (lambda (code) (or (eq code 1) (eq code 0))))))
 
 (use-package magit
   :bind* (("C-S-g" . magit))
--- custom.el
+++ custom.el
@@ -5,8 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
    '((eval when
-	   (fboundp #'tangle-if-init)
-	   (add-hook 'after-save-hook #'tangle-if-init)))))
+           (fboundp #'tangle-if-init)
+           (add-hook 'after-save-hook #'tangle-if-init)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
